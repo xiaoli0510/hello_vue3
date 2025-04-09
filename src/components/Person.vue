f8cd54752afe4fb0d4b8b4a4f430da5c27c3055a
xiaoxiao <253451518@qq.com>
2025-04-10 01:30:33 +0800

    Hooks

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -3,11 +3,16 @@ import useDog from '@/hooks/useDog';
 import useSum from '@/hooks/useSum.ts'
  const {dogList,addDog} = useDog()
 
-const { addSum, sum } = useSum()
+const { addSum, sum,bigSum } = useSum()
+const changeBigSum = () => {
+    bigSum.value = 11
+}
 </script>
 <template>
     <div>sum:{{ sum }}</div>
     <button @click="addSum">增加sum</button>
+    <div>放大100倍的sum:{{ bigSum }}</div>
+    <button @click="changeBigSum">将放大后的sum改为1100</button>
     <br />
 
     <img v-for="(dog, index) in dogList" :key="index" :src="dog" />
--- src/hooks/useSum.ts
+++ src/hooks/useSum.ts
@@ -1,17 +1,28 @@
-import { onMounted, onUpdated, ref } from "vue";
+import { computed, onMounted, onUpdated, ref } from "vue";
 export default function () {
   const sum = ref(0);
   const addSum = () => {
     sum.value += 1;
   };
   onMounted(() => {
-    addSum()
-  })
+    addSum();
+  });
+
+  const bigSum = computed({
+    get() {
+      return sum.value * 100;
+    },
+    set(val) {
+      sum.value = val
+    },
+  });
+
   onUpdated(() => {
-    console.log('更新后了');
-  })
+    console.log("更新后了");
+  });
   return {
     sum,
     addSum,
+    bigSum,
   };
 }
