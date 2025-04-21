51afedb146ada7902a1b4bc9e94f83e1082457d3
xiaoxiao <253451518@qq.com>
2025-04-21 16:18:21 +0800

    v-model通信

--- src/A.vue
+++ src/A.vue
@@ -1,12 +1,17 @@
 <script setup lang='ts'>
-const a = defineModel('a')
-defineModel('b')
+defineProps(['s1', 'y1', 'z1', 'daye1', 'd1'])
+const emit = defineEmits(['update:s1', 'update:y1', 'update:d1', 'update:daye1', 'update:z1'])
 </script>
 <template>
-    <div style="border:1px solid cyan">
-        <div>{{ a }}</div>
-        <button @click="$emit('update:a', '后裔')">改变一下啊</button>
-        <div>{{ b }}</div>
-        <button @click="$emit('update:b', '张飞')">改变一下啊</button>
-    </div>
+    <input :value="s1" @input="emit('update:s1', (<HTMLInputElement>$event.target).value)" />
+    <br />
+    <input :value="y1" @input="emit('update:y1', (<HTMLInputElement>$event.target).value)" />
+    <br />
+    <input :value="z1" @input="emit('update:z1', (<HTMLInputElement>$event.target).value)" />
+    <br />
+    <input :value="daye1" @input="emit('update:daye1', (<HTMLInputElement>$event.target).value)" />
+    <br />
+    <input :value="d1" @input="emit('update:d1', (<HTMLInputElement>$event.target).value)" />
+    <br />
 </template>
+<style scoped lang='scss'></style>
--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <script setup lang='ts'>
 import { ref } from 'vue';
 import A from './A.vue';
-
-const daye = defineModel('a',{
-    default:'李信'
-})
-const zhonglu = defineModel('b',{
-    default:'虞姬'
-})
-
-const change = () => {
-    daye.value = '阿珂'
-}
-
+const s = ref('后裔')
+const y = ref('蔡文姬')
+const d = ref('李信')
+const daye = ref('典韦')
+const z = ref('小乔')
 </script>
 <template>
-    <button @click="change('1')">改变</button>
-    <A v-model:a="daye" v-model:b="zhonglu"></A>
+<A
+v-model:s1="s"
+v-model:y1="y"
+v-model:d1="d"
+v-model:daye1="daye"
+v-model:z1="z"
+@update:s1="s = $event"
+@update:y1="y = $event"
+@update:d1="d = $event"
+@update:daye1="daye = $event"
+@update:z1="z = $event"
+/>
 </template>
-<style scoped lang='scss'></style>
+<style scoped lang='scss'>
+</style>
